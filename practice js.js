67d6aac61452e012decddc5e01d4aed58bea86af
LoveKach <cryptonitik@gmail.com>
2023-10-13 17:52:16 +0300

    feat: add new task

--- practice js.js
+++ practice js.js
@@ -42,17 +42,45 @@
 // }
 
 
+// CW part2 7 task
+
+function findMissing(arr1, arr2) {
+    const countMap = new Map();
+
+
+    for (const num of arr1) {
+        countMap.set(num, (countMap.get(num) || 0) + 1);
+    }
+
+
+    for (const num of arr2) {
+        const count = countMap.get(num);
+        if (count === 1) {
+            countMap.delete(num);
+        } else {
+            countMap.set(num, count - 1);
+        }
+    }
+
+    for (const [key, value] of countMap) {
+        return key;
+    }
+}
+
+
+
+
 // CW part2 8 task
 
 
-function getDecimal(number) {
-    if (typeof number !== 'number') {
-      throw new Error('Invalid input. Please provide a valid number.');
-    }
-  
-    const absoluteNumber = Math.abs(number);
-  
-    const decimalPart = absoluteNumber % 1;
-  
-    return decimalPart;
-  }
+// function getDecimal(number) {
+//     if (typeof number !== 'number') {
+//         throw new Error('Invalid input. Please provide a valid number.');
+//     }
+
+//     const absoluteNumber = Math.abs(number);
+
+//     const decimalPart = absoluteNumber % 1;
+
+//     return decimalPart;
+// }
